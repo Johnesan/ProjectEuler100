8ca53c0358478e037514d71e7257dbba7ed19571
John Esan <jcoool40@gmail.com>
2020-01-20 23:46:40 +0100

    Problem1 done.

---
+++ ProjectEuler100/ProjectEuler100/Problem1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler100
+{
+    /// <summary>
+    /// Multiples of 3 and 5:
+    /// 
+    /// Question: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    ///Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+    /// </summary>
+    public static class Problem1
+    {
+        public static void Solve()
+        {
+            Console.WriteLine("Type in the number:");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine(MultiplesOf3And5(number));
+        }
+
+        public static int MultiplesOf3And5(int number)
+        {
+            int sum = 0;
+            for (var i = number; i >= 3; i--)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ ProjectEuler100/ProjectEuler100/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEuler100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1.Solve();
+        }
+    }
+}
